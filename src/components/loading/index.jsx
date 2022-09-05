897b74a733ad52596f44783055d31b26f1ef1f1d
兰鑫 <1192065030@qq.com>
2022-09-05 13:20:45 +0800

    fix(Loading): 修复loading白框

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Taro from '@tarojs/taro';
-import { View } from '@tarojs/components';
+import { Text } from '@tarojs/components';
 import { AtIcon } from 'taro-ui';
 import './index.scss';
 
@@ -11,16 +11,13 @@ class Loading extends React.PureComponent {
     color: '#aaa',
     icon: undefined,
   };
+
   render() {
     const { name, size, color, icon } = this.props;
     if (icon) {
-      return <View className="loading-component">{icon}</View>;
+      return <Text className="loading-component">{icon}</Text>;
     }
-    return (
-      <View className="loading-component">
-        <AtIcon value={name} size={size} color={color} />
-      </View>
-    );
+    return <AtIcon className="loading-component" value={name} size={size} color={color} />;
   }
 }
 
